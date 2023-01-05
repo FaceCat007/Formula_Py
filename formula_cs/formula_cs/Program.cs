5e692cb3f2c39c38bf7d4e9c7467b3272fdc5152
Todd <facecat007@163.com>
2023-01-05 09:09:42 +0800

    1

--- formula_cs/formula_cs/Program.cs
+++ formula_cs/formula_cs/Program.cs
@@ -15,14 +15,18 @@ namespace formula_cs
 	    recvData 接收数据
 	    */
         [DllImport("facecatcpp.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int calcFormula(String formula, String sendStr, IntPtr recvData);
-        public static int calcFormula(String formula, String sendStr, StringBuilder recvData)
+        public static extern int calcFormulaWithShapes(String formula, String sendStr, IntPtr recvData, IntPtr recvData2);
+        public static int calcFormulaWithShapes(String formula, String sendStr, StringBuilder recvData, StringBuilder recvData2)
         {
             IntPtr bufferIntPtr = Marshal.AllocHGlobal(1024 * 1024 * 10);
-            int state = calcFormula(formula, sendStr, bufferIntPtr);
+            IntPtr bufferIntPtr2 = Marshal.AllocHGlobal(1024 * 1024);
+            int state = calcFormulaWithShapes(formula, sendStr, bufferIntPtr, bufferIntPtr2);
             String sbResult = Marshal.PtrToStringAnsi(bufferIntPtr);
             recvData.Append(sbResult);
+            String sbResult2 = Marshal.PtrToStringAnsi(bufferIntPtr2);
+            recvData2.Append(sbResult2);
             Marshal.FreeHGlobal(bufferIntPtr);
+            Marshal.FreeHGlobal(bufferIntPtr2);
             return state;
         }
 
@@ -92,11 +96,13 @@ namespace formula_cs
         /// <param name="formula">公式</param>
         /// <param name="datas">数据</param>
         /// <returns>结果</returns>
-        public static String calculateFormula(String formula, List<SecurityData> datas)
+        public static String calculateFormula(String formula, List<SecurityData> datas, ref String shapes)
         {
             StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
             String sendStr = securityDatasToStr(datas);
-            calcFormula(formula, sendStr, sb);
+            calcFormulaWithShapes(formula, sendStr, sb, sb2);
+            shapes = sb2.ToString();
             return sb.ToString();
         }
 
@@ -125,7 +131,9 @@ namespace formula_cs
                 }
             }
             //调用计算函数
-            Console.WriteLine(calculateFormula(formula, datas));
+            String shapes = "";
+            Console.WriteLine(calculateFormula(formula, datas, ref shapes));
+            Console.WriteLine(shapes);
             Console.ReadLine();
         }
     }
